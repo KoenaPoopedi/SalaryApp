6a594f70e7ca5bcf8c822f86b781cf95b6e043f7
Koena <Koenapoopedi4.0@gmail.com>
2024-05-24 13:40:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace SalaryApp;
+
+class SalaryApp
+{
+
+    public static void Main(string[] args)
+    {
+        //declearing variables
+        int HoursWorked;
+        double RateOfPay;
+        double Salary;
+        string EmployeeName;
+
+        Appstart:
+        //prompting user to enter their name;
+        Console.WriteLine("Please Enter your name:");
+        EmployeeName = Console.ReadLine();
+
+        //prompting the user to enter their rate of pay
+        Console.WriteLine("Enter your rate:");
+
+        //converting user input into a double digit
+        RateOfPay = Convert.ToDouble(Console.ReadLine());
+
+        //prompting user to enter the hours they have worked.
+        Console.WriteLine("Enter hours worked:");
+
+        //converting user input into an integer digit
+        HoursWorked = Convert.ToInt32(Convert.ToDouble(Console.ReadLine()));
+       
+        //looping the system to continuously calculate the salary
+        while (true)
+        {
+            //app termination
+            if (HoursWorked == -1)
+            {
+                Console.WriteLine("Thank you for using the Salary App");
+                break;
+            }
+
+            //handling exceptions for any invalid character
+            try
+            {
+                if (HoursWorked <= 0)
+                {
+                    Console.WriteLine("You havent been to work this month!!");
+                    goto Appstart;
+                }
+                else if (HoursWorked >= 90)
+                {
+                    Console.WriteLine("You need to take a leave, overtime exedeed!!!!");
+                    goto Appstart;
+                }
+                else
+                {
+                    Salary = RateOfPay * HoursWorked;
+                    Console.WriteLine("Employee Name is:" + N + );
+                    goto Appstart;
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                //exeption message
+                Console.WriteLine("Please Enter a valid entry!!!!");
+                e.ToString();
+            }
+        }
+    }
+}
